505fad5cdf45f5580d9ebe5bfc107da0540cab6b
Milan Sahoo <milansahoo7211662005@gmail.com>
2025-02-09 17:47:29 +0530

    Update Footer.jsx

--- src/assets/componants/Footer.jsx
+++ src/assets/componants/Footer.jsx
@@ -3,7 +3,7 @@ import "../css/Footer.css";
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; {new Date().getFullYear()} Your Name. All Rights Reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Milan (M.S). All Rights Reserved.</p>
             <div className="footer-links">
                 <a href="https://github.com/yourgithubhttps://github.com/Milan721166" target="_blank" rel="noopener noreferrer">GitHub</a>
                 <a href="https://www.linkedin.com/in/milan-sahoo-98a912312" target="_blank" rel="noopener noreferrer">LinkedIn</a>

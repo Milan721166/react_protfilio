4a65126c8039d16534ad6cb4a402626ceaa004c3
Milan Sahoo <milansahoo7211662005@gmail.com>
2025-05-06 08:40:23 +0530

    Update Footer.jsx

--- src/assets/componants/Footer.jsx
+++ src/assets/componants/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="footer">
             <p>&copy; {new Date().getFullYear()} Milan (M.S). All Rights Reserved.</p>
             <div className="footer-links">
-                <a href="https://github.com/yourgithubhttps://github.com/Milan721166" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <a href="https://github.com/Milan721166" target="_blank" rel="noopener noreferrer">GitHub</a>
                 <a href="https://www.linkedin.com/in/milan-sahoo-98a912312" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                 <a href="mailto:milansahoo721166205@gmail.com">Email</a>
             </div>
